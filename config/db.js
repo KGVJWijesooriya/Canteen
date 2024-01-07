fa78fd05c43be0157df4958e0c4dddcf1f28e6bb
Vimukthi Jayasanka <vimukthi200020@gmail.com>
2024-01-08 01:03:18 +0530

    updated mongodb url

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/canteen').on('open',()=>{
+const connection = mongoose.createConnection('mongodb+srv://vimukthi200007:Vimukthi27@canteen.kvebs8e.mongodb.net/?retryWrites=true&w=majority').on('open',()=>{
     console.log("MongoDb Connected");
 }).on('error',()=>{
     console.log("MongoDB Connection error");
--- model/user_model.js
+++ model/user_model.js
@@ -1,21 +1,35 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+
 const db = require('../config/db');
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    email:{
-        type:String,
-        lowercase:true,
-        require :true,
-        unique:true
+    email: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        require :true
+    password: {
+        type: String,
+        required: true
     }
 });
 
-const UserModel = db.model('user',userSchema);
+userSchema.pre('save', async function () {
+    try {
+        var user = this;
+        const salt = await bcrypt.genSalt(10);
+        const hashpass = await bcrypt.hash(user.password, salt);
+
+        user.password = hashpass;
+    } catch (error) {
+        throw error;
+    }
+});
+
+const UserModel = db.model('user', userSchema);
 
 module.exports = UserModel;
