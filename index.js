a38081ddd750bbec866181186073b2892b24188b
Vimukthi Jayasanka <vimukthi200020@gmail.com>
2024-01-10 17:09:56 +0530

    login connect

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
+// const app = require('./app');
+// const db = require('./config/db');
+// const UserModel = require('./model/user_model');
+
+// const port = process.env.PORT || 3000;
+
+// app.get('/', (req, res) => {
+//   res.send("Hello Pemini")
+// });
+
+// db.connect()
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(`Server listening on port ${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error('Error connecting to the database:', err);
+//   });
+
 const app = require('./app');
 const db = require('./config/db');
-const UserModel = require('./model/user_model');
 
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000; 
-
-app.get('/', (req, res) => {
-    res.send("Hello Pemini")
-});
-
-// db.connect() // Assuming this is how you connect to your database, adjust as needed
-//     .then(() => {
-        app.listen(port, () => {
-            console.log(`Server listening on port ${port}`);
-        });
-//     })
-//     .catch((err) => {
-//         console.error('Error connecting to the database:', err);
-//     });
+// db.connect()
+//   .then(() => {
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+//   })
+//   .catch((err) => {
+//     console.error('Error connecting to the database:', err);
+//   });
